9bc4c9ba64bdb94cb150ff2d3187bde06f806093
BhabhaSeptember <septemberbhabha@gmail.com>
2024-01-23 20:20:35 +0200

    edits

--- 3. Ch2 (JS Essentials)/3. Ch2.js
+++ 3. Ch2 (JS Essentials)/3. Ch2.js
@@ -530,26 +530,3 @@ console.log(a == 3 || b == 7);
 
 
 
-                                        /* READY FOR REVIEW */
-
-
-// //CHAPTER PROJECT
-// //MILES-TO-KILOMETERS
-// let kms = prompt("Enter distance in kms below to begin conversion: ");
-// let miles = Math.floor(kms * 202.2142);
-// console.log("The distance of " + kms + "km is equal to " + miles + " miles.");
-
-
-// //BMI CALCULATOR
-let heightInches = prompt("Type your height in inches: ");
-let weightPounds = prompt("Type your weight in pounds: ");
-// console.log("Height in Inches = " + heightInches);
-// console.log("Weight in Pounds = " + weightPounds);
-
-let heightCm = heightInches * 2.54 
-let weightKgs = weightPounds / 2.2046;
-// console.log("Height in Cm's = " + heightCm);
-// console.log("Weight in Kilograms = " + weightKgs);
-
-let bmi = weightKgs / (heightCm/100 * heightCm/100);
-console.log(bmi);
