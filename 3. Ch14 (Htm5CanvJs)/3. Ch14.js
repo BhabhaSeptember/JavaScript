fb5bdecde474c81ab6779c96e164b805993cebf8
BhabhaSeptember <septemberbhabha@gmail.com>
2024-01-23 20:09:38 +0200

    ch14 projects

--- 3. Ch14 (Htm5CanvJs)/3. Ch14.js
+++ 3. Ch14 (Htm5CanvJs)/3. Ch14.js
@@ -3,18 +3,18 @@
 // if (window.FileReader) {
 //   message.innerText = "Good to go!";
 // } else {
-//   message.innerText = "No FileReader :(";
+//   message.innerText = "No FileReader :( ";
 // }
 
 //UPLOADING FILES (SEE HTML)
-//EXAMPLE 1 (SINGLE FILE)
+// EXAMPLE 1 (SINGLE FILE)
 // let message = document.getElementById("message");
 // function uploadFile(files) {
 //   console.log(files[0]);
 //   message.innerText = files[0].name;
 // }
 
-//EXAMPLE 2 (MULTIPLE FILES)
+// EXAMPLE 2 (MULTIPLE FILES)
 // let message = document.getElementById("message");
 // function uploadFile(files) {
 //   for (let i = 0; i < files.length; i++) {
@@ -161,7 +161,6 @@
 //  ctx.font = "24px Arial";
 //  let txt = "A canvas!";
 // ctx.fillText(txt, 10, 35);  //(text, x position, y position)
-
 //  ctx.textAlign = "center";
 
 //PRACTICE EXERCISE 14.4
@@ -181,35 +180,35 @@
 
 //ADDING & UPLOADING IMAGES TO THE CANVAS
 //EXAMPLE 1
-//Below ensures img is loaded before getting it from DOM or else canvas will remain empty
+// //Below ensures img is loaded before getting it from DOM or else canvas will remain empty
 // window.onload = function () {
 //   let canvas = document.getElementById("c1");
+//   let ctx = canvas.getContext("2d");
 //   canvas.height = 500;
 //   canvas.width = 1000;
-//   let ctx = canvas.getContext("2d");
 //   let myImage = document.getElementById("flower");
 //   ctx.drawImage(myImage, 10, 10);  // (img, x position, y position)
 // };
 
-//EXAMPLE 2
-//USING ONE CANVAS INSIDE ANOTHER CANVAS
+// //EXAMPLE 2
+// //USING ONE CANVAS INSIDE ANOTHER CANVAS
 // let canvas1 = document.getElementById("canvas1");
 // let ctx1 = canvas1.getContext("2d");
 // ctx1.strokeRect(5, 5, 150, 100);  //(x, y, width, height)
 
-// let canvas2 = document.getElementById("canvas2");
+// // let canvas2 = document.getElementById("canvas2");
 // let ctx2 = canvas2.getContext("2d");
 // ctx2.beginPath();
 // ctx2.arc(60, 60, 20, 0, 2 * Math.PI); // (x, y, radius, start angle, end angle)
 // ctx2.stroke();
 
-// let canvas3 = document.getElementById("canvas3");
+// // let canvas3 = document.getElementById("canvas3");
 // let ctx3 = canvas3.getContext("2d");
 // ctx3.drawImage(canvas1, 10, 10);  // (img, x, y)
 // ctx3.drawImage(canvas2, 10, 10);
 
-//EXAMPLE 3
-//UPLOADING IMAGES TO CANVAS
+// //EXAMPLE 3
+// //UPLOADING IMAGES TO CANVAS
 // let canvas = document.getElementById("canvas");
 // let ctx = canvas.getContext("2d");
 // let imgLoader = document.getElementById("imgLoader");
@@ -258,12 +257,12 @@
 //ADDING ANIMATIONS TO THE CANVAS
 // window.onload = init;
 
-// var canvas = document.getElementById("canvas");
-// var ctx = canvas.getContext("2d");
+// let canvas = document.getElementById("canvas");
+// let ctx = canvas.getContext("2d");
 // canvas.height = 500;
 // canvas.width = 500;
 
-// var pos = {
+// let pos = {
 //   x: 0,
 //   y: 50,
 // };
@@ -273,7 +272,7 @@
 // }
 // function draw() {
 //   //clears entire canvas including previously drawn square to create illusion of animation
-//   ctx.clearRect(0, 0, canvas.width, canvas.height);
+//   // ctx.clearRect(0, 0, canvas.width, canvas.height);
 //   pos.x = pos.x + 5; //creates new starting x position after every 50ms
 //   if (pos.x > canvas.width) {
 //     pos.x = 0;
@@ -385,6 +384,183 @@
 //   ctx.fillRect(70, 70, 100, 100);
 // }
 
-
-
 //MEDIA ON PAGE (SEE HTML)
+
+// ------------------------------------------------------------------------------------------
+
+//CHAPTER PROJECTS
+//1. MATRIX EFFECT
+// const canvas = document.createElement("canvas");
+// const ctx = canvas.getContext("2d");
+// canvas.setAttribute("width", "500");
+// canvas.setAttribute("height", "400");
+// document.body.prepend(canvas);
+
+// const colVal = [];
+// for (let x = 0; x < 50; x++) { // # of value 0 items to be added to array (10px between each column)
+//   colVal.push(0);
+// }
+
+// function matrix() {
+//   ctx.fillStyle = "rgba(0,0,0,.05)"; //opacity produces fading effect when layered ontop of existing elements
+//   ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+//   ctx.fillStyle = "green"; //canvas font color
+
+//   colVal.map((posY, index) => { //array holds vertical color position for output text
+//     let output = Math.random() < 0.5 ? 0 : 1;
+//     let posX = index * 10 + 10; //setting separated columns for each new character
+//     //below fills canvas
+//     ctx.fillText(output, posX, posY); //zero's in array are starting vertical position for content
+
+//     //below: larger numbers fall further down y-axis
+//     //random number creates staggered effect
+//     if (posY > 100 + Math.random() * 300) {
+//       colVal[index] = 0;
+//     } else {
+//       //below moves letter down 10px on canvas within next draw round
+//       colVal[index] = posY + 10;
+//     }
+//   });
+// }
+// setInterval(matrix, 50); //matrix to run at 50ms intervals
+
+//2. COUNTDOWN CLOCK
+// const endDate = document.querySelector("input");
+// const clock = document.querySelector(".clock");
+// let timeInterval;
+// let timeStop = true;
+
+// //below: check if item for countdown already set,
+// const savedValue = localStorage.getItem("countdown") || false;
+// // if there is saved item --> use that value
+// if (savedValue) {
+//   startClock(savedValue);
+//   let inputValue = new Date(savedValue);
+//   endDate.valueAsDate = inputValue; //input field date value set to local storage saved val
+// }
+
+// endDate.addEventListener("change", function (e) { //if input field is changed
+//   e.preventDefault();
+//   clearInterval(timeInterval);
+//   const temp = new Date(endDate.value);
+//   localStorage.setItem("countdown", temp); //set new endDate value in local storage
+//   startClock(temp); //start clock function from new endDate value
+//   timeStop = true;
+// });
+
+// function startClock(d) { //function starts clock that is used to start counter
+//   //below function updates counter
+//   function updateCounter() {
+//     let tl = timeLeft(d);
+//     if (tl.total <= 0) {
+//       timeStop = false; //stops the timer
+//     }
+//     for (let pro in tl) {
+//       let el = clock.querySelector("." + pro);
+//       if (el) {
+//         el.innerHTML = tl[pro];
+//       }
+//     }
+//   }
+//   //below: if timeStop is true we continue updating counter every second otherwise we stop
+//   updateCounter();
+//   if (timeStop) {  //timeStop = true i.e. continue counting down
+//     timeInterval = setInterval(updateCounter, 1000);
+//   } else {
+//     clearInterval(timeInterval);
+//   }
+// }
+
+// function timeLeft(d) {
+//   let currentDate = new Date();
+//   let t = Date.parse(d) - Date.parse(currentDate);
+//   let seconds = Math.floor((t / 1000) % 60);
+//   let minutes = Math.floor((t / 1000 / 60) % 60);
+//   let hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+//   let days = Math.floor(t / (1000 * 60 * 60 * 24));
+//   return {
+//     total: t,
+//     days: days,
+//     hours: hours,
+//     minutes: minutes,
+//     seconds: seconds,
+//   };
+// }
+
+// //3. ONLINE PAINT APP
+const canvas = document.querySelector("#canvas");
+const ctx = canvas.getContext("2d");
+const penColor = document.querySelector("#penColor");
+const penWidth = document.querySelector("#penWidth");
+const btnSave = document.querySelector(".save");
+const btnClear = document.querySelector(".clear");
+const output = document.querySelector(".output");
+
+const mLoc = { //tracks location of pen
+  draw: false,
+  x: 0,
+  y: 0,
+  lastX: 0,
+  lastY: 0,
+};
+
+canvas.style.border = "1px solid black";
+btnSave.addEventListener("click", saveImg);
+btnClear.addEventListener("click", clearCanvas);
+
+canvas.addEventListener("mousemove", (e) => {
+  mLoc.lastX = mLoc.x; //updates pen position to lastX position
+  mLoc.lastY = mLoc.y;
+  //console.log(e);
+  mLoc.x = e.clientX; //sets location position to clientX
+  mLoc.y = e.clientY;
+  draw();
+});
+
+canvas.addEventListener("mousedown", (e) => {
+  mLoc.draw = true;
+});
+canvas.addEventListener("mouseup", (e) => {
+  mLoc.draw = false;
+});
+canvas.addEventListener("mouseout", (e) => {
+  mLoc.draw = false;
+});
+
+function draw() {
+  if (mLoc.draw) {
+    ctx.beginPath();
+    ctx.moveTo(mLoc.lastX, mLoc.lastY);
+    ctx.lineTo(mLoc.x, mLoc.y);
+    ctx.strokeStyle = penColor.value;
+    ctx.lineWidth = penWidth.value;
+    ctx.stroke();
+    ctx.closePath();
+  }
+}
+
+function saveImg() {
+  const dataURL = canvas.toDataURL();
+  console.log(dataURL);
+  const img = document.createElement("img");
+  output.prepend(img);
+  img.setAttribute("src", dataURL);
+  const link = document.createElement("a"); //to set download must create an anchor tag
+  output.append(link);
+  let fileName = Math.random().toString(16).substr(-8) + ".png";
+  link.setAttribute("download", fileName); //hyperlink set to download attribute
+  link.href = dataURL;
+  link.click();
+  output.removeChild(link);
+}
+
+
+function clearCanvas() {
+  let temp = confirm("Are you sure you want to clear the canvas? Changes will not be saved.");
+  if (temp) {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+  }
+}
+
+
